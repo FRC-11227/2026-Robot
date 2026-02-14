be309b8b043bd19175a42f69c73697e9a054f718
LavaDiamond <Emfische@outlook.com>
2026-02-14 17:49:26 -0500

    update

---
+++ src/main/java/frc/robot/subsystems/intake.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.Kraken X60Config;
+import com.revrobotics.spark.Kraken X60;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.BallConstants;
+import frc.robot.Constants.CANConstants;
+import BallSubsystem;
+public class intake {
+    
+}
