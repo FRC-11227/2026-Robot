7f1718bb9500f2d9b466e33c8ef812690fcad89e
Erz <eric77chen@gmail.com>
2026-03-06 18:45:32 -0500

    nitrogen path

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -7,15 +7,26 @@ package frc.robot.commands;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
 
 public final class Autos {
-  /** Example static factory for an autonomous command. */
-  public static Command exampleAuto(ExampleSubsystem subsystem) {
-    return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
+  public static Command nitrogenAuto() {
+    try {
+      PathPlannerPath path = PathPlannerPath.fromPathFile("Nitrogen path");
+      return AutoBuilder.followPath(path);
+    } catch(Exception err) {
+      return Commands.none();
+    }
   }
   // public static Command rotateToTag(CommandSwerveDrivetrain swerveDrivetrain) {
   //   return swerveDrivetrain.rotateToTarget();
   // }
+
+  /** Example static factory for an autonomous command. */
+  public static Command exampleAuto(ExampleSubsystem subsystem) {
+    return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
+  }
 
   private Autos() {
     throw new UnsupportedOperationException("This is a utility class!");
