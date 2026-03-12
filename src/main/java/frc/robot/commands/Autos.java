0fd34709426e6f26436c1db898ff0d8df7ea1c38
Mayibane10 <mayibane10@gmail.com>
2026-03-12 18:56:14 -0400

    Tweak Autos

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -4,18 +4,30 @@
 
 package frc.robot.commands;
 
+import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
+
 public final class Autos {
+
+  // orange Note: Add commands with "static"
+  public static Command AlignToNets(CommandSwerveDrivetrain swerveDrivetrain) {
+    try {
+      PathPlannerPath path = PathPlannerPath.fromPathFile("Allign to nets");
+      return AutoBuilder.followPath(path);
+    } catch (Exception err) {
+      return Commands.none();
+    }
+  }
+
   /** Example static factory for an autonomous command. */
   public static Command exampleAuto(ExampleSubsystem subsystem) {
     return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
   }
-  // public static Command rotateToTag(CommandSwerveDrivetrain swerveDrivetrain) {
-  // return swerveDrivetrain.rotateToTarget();
-  // }
 
   private Autos() {
     throw new UnsupportedOperationException("This is a utility class!");
