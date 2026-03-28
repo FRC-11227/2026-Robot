a94339d8af485896bcfad4e7bd2bae4ad064efab
ljewer <liam@jewer.ca>
2026-03-27 23:57:48 -0400

    automatic tab switching

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,6 +17,8 @@ import edu.wpi.first.epilogue.logging.errors.ErrorHandler;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -24,6 +26,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 @Logged(strategy = Strategy.OPT_IN)
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
+
 
 
     private final RobotContainer m_robotContainer;
@@ -84,6 +87,8 @@ public class Robot extends TimedRobot {
     @Override
     public void autonomousInit() {
         match_timer.auto_init();
+
+        Shuffleboard.selectTab(1);
         
         m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
@@ -101,6 +106,8 @@ public class Robot extends TimedRobot {
     @Override
     public void teleopInit() {
         match_timer.tele_init();
+        
+        Shuffleboard.selectTab(0);
 
         if (m_autonomousCommand != null) {
             CommandScheduler.getInstance().cancel(m_autonomousCommand);
